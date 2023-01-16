7e53669ac867a7052e6bd518489a9e7ba937c9d8
Raul <raulrufino0@gmail.com>
2023-01-16 08:55:41 -0300

    Add simple rotation in avl tree

--- src/avltree.cpp
+++ src/avltree.cpp
@@ -6,6 +6,32 @@ template <typename T> bool insert(Tree<T>* tree, T value) {
     return node != nullptr;
 }
 
+template <typename T> void rotate_left(Tree<T>** tree) {
+    Tree<T>* right = (*tree)->right;
+    Tree<T>* right_left = right->left;
+    if (right_left != nullptr) {
+        (*tree)->set_right(right_left);
+    } else {
+        (*tree)->right = nullptr;
+    }
+    right->father = (*tree)->father;
+    right->set_left(*tree);
+    *tree = right;
+}
+
+template <typename T> void rotate_right(Tree<T>** tree) {
+    Tree<T>* left = (*tree)->left;
+    Tree<T>* left_right = left->right;
+    if (left_right != nullptr) {
+        (*tree)->set_left(left_right);
+    } else {
+        (*tree)->left = nullptr;
+    }
+    left->father = (*tree)->father;
+    left->set_right(*tree);
+    *tree = left;
+}
+
 template <typename T> Tree<T>* insert_without_rebalance(Tree<T>* tree, T info) {
     Tree<T>* subnode = tree;
     Tree<T>* subnode_father = tree->father;
@@ -32,4 +58,6 @@ template <typename T> Tree<T>* insert_without_rebalance(Tree<T>* tree, T info) {
 }
 
 template Tree<int>* insert_without_rebalance<int> (Tree<int>*, int);
-template bool insert<int> (Tree<int>* tree, int value);
+template void rotate_left<int> (Tree<int>**);
+template void rotate_right<int> (Tree<int>**);
+template bool insert<int> (Tree<int>*, int);
--- src/avltree.h
+++ src/avltree.h
@@ -6,5 +6,7 @@
 
 template <typename T> bool insert(Tree<T>* tree, T value);
 template <typename T> Tree<T>* insert_without_rebalance(Tree<T>* tree, T info);
+template <typename T> void rotate_left(Tree<T>** tree);
+template <typename T> void rotate_right(Tree<T>** tree);
 
 #endif // AVLTREE_H_
