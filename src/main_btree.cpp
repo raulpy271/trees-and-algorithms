e1cf2986c9ca0551746a5c9ca2b9a1e3412b1320
Raul <raulrufino0@gmail.com>
2023-02-25 14:28:41 -0300

    Simplify btree main example

--- src/main_btree.cpp
+++ src/main_btree.cpp
@@ -1,36 +1,27 @@
 
-#include "btree.h"
-
 #include <iostream>
 
-void exemplo_insercao() {
-    BTree<int, 3>* bt = new BTree<int, 3>();
-    for (int i = 8; i > 0; i--) {
-        std::cout << "inserindo " << i << std::endl;
-        bt = bt->insert(i);
-        std::cout << "resutado: " << std::endl;
-        std::cout << bt->tree_repr([=] (int key) {return std::to_string(key);});
-        std::cout << "\n";
-    }
-}
+#include "btree.h"
 
-void exemplo_remocao() {
-    int to_remove = 3;
-    BTree<int, 3>* bt = new BTree<int, 3>();
-    for (int i = 33; i > 0; i--) {
-        bt = bt->insert(i);
-    }
-    std::cout << "arvore original: " << std::endl;
-    std::cout << bt->tree_repr([=] (int key) {return std::to_string(key);});
-
-    std::cout << "\nRemovendo: " << to_remove << std::endl;
-
-    bt = bt->delete_key(to_remove);
-
-    std::cout << bt->tree_repr([=] (int key) {return std::to_string(key);});
+void print_tree(BTree<int, 3>* bt) {
+    std::cout << "Result: " << std::endl;
+    std::cout << bt->tree_repr([=] (int key) {
+        return std::to_string(key);
+    });
 }
 
 int main() {
-    //exemplo_insercao();
-    exemplo_remocao();
+    BTree<int, 3>* bt = new BTree<int, 3>();
+
+    for (int i = 0; i < 15; i++) {
+        std::cout << "Inserting " << i << std::endl;
+        bt = bt->insert(i);
+    }
+
+    print_tree(bt);
+
+    std::cout << "Deleting 10" << std::endl;
+    bt = bt->delete_key(10);
+
+    print_tree(bt);
 }
